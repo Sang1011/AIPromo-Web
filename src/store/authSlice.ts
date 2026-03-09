9995071e8c85dc3effda3fa0bbd4a76ec2e24756
ChiThuan <tranchithuan@gmail.com>
2026-03-09 13:50:10 +0700

    call API and show store

---
+++ src/store/authSlice.ts
@@ -0,0 +1,61 @@
+import { createAsyncThunk, createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import authService from "../services/authService";
+import type { LoginRequest } from "../types/auth/auth";
+
+
+const name = "auth";
+
+interface AuthState {
+   token: string | null;
+   currentInfor: object;
+}
+
+const initialState: AuthState = {
+   token: null,
+   currentInfor :{}
+};
+
+export const fetchLogin = createAsyncThunk<
+   any,
+   LoginRequest
+>(
+   `${name}/fetchLogin`,
+   async (params, thunkAPI) => {
+      try {
+         const response = await authService.login(params);
+         return response.data;
+      } catch (error) {
+         return thunkAPI.rejectWithValue(error);
+      }
+   }
+);
+
+export const fetchLoginGoogle = createAsyncThunk(`${name}/fetchLoginGoogle`, async(data: object,thunkAPI)=>{
+     try {
+         const response = await authService.loginGoogle(data);
+         return response.data;
+      } catch (error) {
+         return thunkAPI.rejectWithValue(error);
+      }
+})
+
+const authSlice = createSlice({
+   name,
+   initialState,
+   reducers: {},
+   extraReducers: (builder) => {
+      builder.addCase(fetchLogin.fulfilled, (state, action: PayloadAction<any>) => {
+         const responseData = action.payload?.data;
+         if (responseData?.payload?.isSuccess === true) {
+            const token = responseData.accessToken;
+            state.currentInfor = action.payload?.user;
+            state.token = token;
+            if (token) {
+               localStorage.setItem("ACCESS_TOKEN", token);
+            }
+         }
+      });
+   },
+});
+
+export default authSlice.reducer;
