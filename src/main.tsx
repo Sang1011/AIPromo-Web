b665988267c63c68d387b15f9c42e08d92d96f92
ChiThuan <tranchithuan@gmail.com>
2026-03-09 13:46:45 +0700

    add Provider  GoogleOAuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store/index.ts'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
+console.log();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Provider store={store}>
+    <GoogleOAuthProvider  clientId={clientId}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+    </GoogleOAuthProvider>
+  </Provider>,
 )
