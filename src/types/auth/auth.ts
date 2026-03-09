7cbd4ea161e211cac8b04ec52598fe18ae918a35
ChiThuan <tranchithuan@gmail.com>
2026-03-09 15:28:50 +0700

    create interface RegisterRequest

--- src/types/auth/auth.ts
+++ src/types/auth/auth.ts
@@ -6,3 +6,13 @@ export interface LoginRequest {
   ipAddress: string
   userAgent: string
 }
+
+export interface RegisterRequest {
+  email: string;
+  userName: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  address: string;
+}
