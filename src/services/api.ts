4ce94e3ca1c1105a99cace09eb0adfbedd5de700
ChiThuan <tranchithuan@gmail.com>
2026-03-11 11:40:26 +0700

    add call with token

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,6 +7,15 @@ const API = {
       baseURL: import.meta.env.VITE_API_BASE_URL,
     });
   },
+  callWithToken: function (token : string): AxiosInstance {
+    const accessToken = token ?? localStorage.getItem("ACCESS_TOKEN") ?? "";
+    return axios.create({
+      baseURL: import.meta.env.VITE_API_BASE_URL,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  },
 };
 
 export default API;
