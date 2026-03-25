04c475ee0a3028bdd5e50998580f4e8a19690322
ChiThuan <tranchithuan@gmail.com>
2026-03-25 16:21:02 +0700

    change respone

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,12 +1,12 @@
 import type { AxiosResponse } from "axios";
 import { interceptorAPI } from "../utils/attachInterceptors";
-import type { GetAllOrderRequest } from "../types/order/order";
+import type { GetAllOrderRequest, GetAllOrderResponse } from "../types/order/order";
 
 const orderService = {
    getDetailOrder: (orderId: string): Promise<AxiosResponse<any>>=>{
     return interceptorAPI().get(`/ticketing/${orderId}`)
    },
-   getAllOrder: (data: GetAllOrderRequest): Promise<AxiosResponse<any>>=>{
+   getAllOrder: (data: GetAllOrderRequest): Promise<AxiosResponse<GetAllOrderResponse>>=>{
        return interceptorAPI().get("/ticketing", {
             params: data
         })
