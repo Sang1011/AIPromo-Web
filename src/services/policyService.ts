3b4c37529d29363f42a88721f843582bf3befaad
ChiThuan <tranchithuan@gmail.com>
2026-04-08 21:01:45 +0700

    postPolicy và postPolicyfile

--- src/services/policyService.ts
+++ src/services/policyService.ts
@@ -1,13 +1,22 @@
 import type { AxiosResponse } from "axios";
 import { interceptorAPI } from "../utils/attachInterceptors";
-import type { GetAllPoliciesResponse, GetPolicyByIdResponse } from "../types/policy/policy";
+import type { GetAllPoliciesResponse, GetPolicyByIdResponse, PolicyRequest } from "../types/policy/policy";
+
 const policyService = {
     getAllPolicies: (): Promise<AxiosResponse<GetAllPoliciesResponse>> => {
         return interceptorAPI().get("/policies");
     },
     getPolicyById: (id: string): Promise<AxiosResponse<GetPolicyByIdResponse>> => {
         return interceptorAPI().get(`/policies/${id}`);
-    }
+    },
+    postPolicy: (data: PolicyRequest): Promise<AxiosResponse<any>> => {
+        return interceptorAPI().post(`/policies`, data);
+    },
+    postPolicyfile: (data: { file: File }): Promise<AxiosResponse<any>> => {
+         const formData = new FormData();
+         formData.append("file", data.file);
+        return interceptorAPI().post(`/policies/upload`, formData);
+    },
 }
 
 export default policyService;
