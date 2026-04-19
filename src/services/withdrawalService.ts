69b5c3d213a9ebb4dcb65589ad869af0771bbd24
ChiThuan <tranchithuan@gmail.com>
2026-04-19 14:55:03 +0700

    call api GetWithdrawalme và GetWithdrawalmeDetail

--- src/services/withdrawalService.ts
+++ src/services/withdrawalService.ts
@@ -1,6 +1,6 @@
 import type { AxiosResponse } from "axios";
 import { interceptorAPI } from "../utils/attachInterceptors";
-import type { CreateWithdrawal } from "../types/withdrawal/withdrawal";
+import type { CreateWithdrawal, GetMeWithdrawalParams } from "../types/withdrawal/withdrawal";
 import type { WithdrawalApiResponse, WithdrawalQueryParams, WithdrawalDetailApiResponse, WithdrawalActionRequest, WithdrawalActionApiResponse } from "../types/withdrawal/withdrawal";
 
 const withdrawalService = {
@@ -19,8 +19,18 @@ const withdrawalService = {
     completeWithdrawal: (id: string, data: WithdrawalActionRequest): Promise<AxiosResponse<WithdrawalActionApiResponse>> => {
         return interceptorAPI().put(`/admin/withdrawal-requests/${id}/complete`, data);
     },
-    createWithdrawal: (data: CreateWithdrawal  ): Promise<AxiosResponse<any>> => {
-        return interceptorAPI().post(`withdrawal-requests`,data);
+    createWithdrawal: (data: CreateWithdrawal): Promise<AxiosResponse<any>> => {
+        return interceptorAPI().post(`withdrawal-requests`, data);
+    },
+    GetWithdrawalme: (
+        params: GetMeWithdrawalParams
+    ): Promise<AxiosResponse<any>> => {
+        return interceptorAPI().get(`withdrawal-requests/me`, {
+            params: params,
+        });
+    },
+    GetWithdrawalmeDetail: (id: string): Promise<AxiosResponse<any>> => {
+        return interceptorAPI().get(`withdrawal-requests/me/${id}`);
     },
 };
 
